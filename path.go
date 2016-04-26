b66ff86e459ddae1b5c8edec04869af9647fadd7
Maxime Vidori <maxime.vidori@gmail.com>
2016-04-26 10:13:16 +0200

    Add features and remove the pointer

--- path.go
+++ path.go
@@ -13,90 +13,89 @@ var POSIX = runtime.GOOS != "windows"
 
 type Path string
 
-func createPointer(path string) *Path {
-	p_path := new(Path)
-	*p_path = Path(path)
-	return p_path
+func (self Path) Abs() (Path, error) {
+	path, err := filepath.Abs(string(self))
+	return Path(path), err
 }
 
-func (self *Path) Abs() (*Path, error) {
-	path, err := filepath.Abs(string(*self))
-	if err != nil {
-		return nil, err
-	}
-	return createPointer(path), nil
-}
-
-func (self *Path) NormCase() *Path {
+func (self Path) NormCase() Path {
 	if !POSIX {
-		return createPointer(strings.ToLower(string(*self)))
+		return Path(strings.ToLower(string(self)))
 	}
 	return self
 }
 
-func (self *Path) NormPath() *Path {
-	return createPointer(filepath.Clean(string(*self)))
+func (self Path) NormPath() Path {
+	return Path(filepath.Clean(string(self)))
 }
 
-func (self *Path) RealPath() (*Path, error) {
-	path, err := filepath.EvalSymlinks(string(*self))
-	if err != nil {
-		return nil, err
-	}
-	return createPointer(path), nil
+func (self Path) RealPath() (Path, error) {
+	path, err := filepath.EvalSymlinks(string(self))
+	return Path(path), err
 }
 
-func expandPosixUser(path string) (*Path, error) {
+func expandPosixUser(path string) string {
+	var home string
 	i := strings.IndexRune(path, os.PathSeparator)
 	switch {
 	case i == -1:
 		i = len(path)
+		fallthrough
 	case i == 1:
-		home := os.Getenv("HOME")
+		home = os.Getenv("HOME")
 		if home == "" {
 			u, err := user.Current()
 			if err != nil {
-				return nil, err
+				return path
 			}
 			home = u.HomeDir
 		}
 	default:
+		u, err := user.Lookup(path[1:i])
+		if err != nil {
+			return path
+		}
+		home = u.HomeDir
 	}
-	return nil, nil
+	return home + path[i:]
 }
 
-func expandNtUser(path string) (*Path, error) {
-	return nil, nil
+func expandNtUser(path string) string {
+	return path
 }
 
-func (self *Path) ExpandUser() (*Path, error) {
-	path := string(*self)
+func (self Path) ExpandUser() Path {
+	path := string(self)
 	if path[0] != '~' {
-		return self, nil
-	}
-	if POSIX {
-		return expandPosixUser(path)
+	} else if POSIX {
+		path = expandPosixUser(path)
 	} else {
-		return expandNtUser(path)
+		path = expandNtUser(path)
 	}
+
+	return Path(path)
+}
+
+func (self Path) ExpandVars() Path {
+	return self
+}
+
+func (self Path) DirName() Path {
+	return Path(filepath.Dir(string(self)))
 }
 
 func main() {
-	p := Path(".")
-	v, _ := p.Abs()
-	fmt.Printf("%v\n", *v)
+	v, _ := Path(".").Abs()
+	fmt.Printf("%s\n", v)
 
-	p = Path("./Toto")
-	v = p.NormCase()
-	fmt.Printf("%v\n", *v)
+	fmt.Printf("%s\n", Path("./Toto").NormCase())
+	fmt.Printf("%s\n", Path("..//streams/.").NormPath())
 
-	p = Path("..//streams/.")
-	v = p.NormPath()
-	fmt.Printf("%v\n", *v)
+	v, _ = Path("./toto").RealPath()
+	fmt.Printf("%s\n", v)
 
-	p = Path("./toto")
-	v, _ = p.RealPath()
-	fmt.Printf("%v\n", *v)
-	// RealPath
-	// ExpandUser
+	fmt.Printf("%s\n", Path("~/toto").ExpandUser())
+
+	// ExpandVars
+	fmt.Printf("%s\n", Path("/toto/titi.go").DirName())
 }
