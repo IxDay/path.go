6c97fa74f572004e6a296676b45705d91c7f5788
Maxime Vidori <maxime.vidori@gmail.com>
2016-04-29 11:49:59 +0200

    Complete test coverage on shutil module

--- errors.go
+++ errors.go
@@ -3,6 +3,7 @@ package path
 import (
 	"errors"
 	"fmt"
+	"os"
 )
 
 var NotAFileError = errors.New("not a file")
@@ -11,4 +12,18 @@ type WarningError []Path
 
 func (self *WarningError) Error() string {
 	return fmt.Sprintf("%d directories skipped", len(*self))
+}
+
+func IsAFileError(err error) bool {
+	switch pe := err.(type) {
+	case nil:
+		return false
+	case *os.PathError:
+		err = pe.Err
+	case *os.LinkError:
+		err = pe.Err
+	case *os.SyscallError:
+		err = pe.Err
+	}
+	return err == NotAFileError
 }
--- shutil_test.go
+++ shutil_test.go
@@ -2,7 +2,6 @@ package path
 
 import (
 	"os"
-	"reflect"
 	"testing"
 )
 
@@ -26,11 +25,68 @@ func TestRemove(t *testing.T) {
 	// test delete dir behavior
 	TmpDir_(func(p Path) {
 		d, _ := p.TempDir_()
-		err := d.Remove().(*os.PathError)
-		if !reflect.DeepEqual(err.Err, NotAFileError) {
+		if err := d.Remove(); !IsAFileError(err) {
 			t.Errorf(
 				"Path('/some/dir').Remove() must return a is not file error, got %q",
 				err,
+			)
+		}
+	})
+}
+
+func TestRemoveP(t *testing.T) {
+	var f *File
+	TmpDir_(func(p Path) {
+		// test normal behavior
+		f, _ = p.TempFile_()
+		if err := f.RemoveP(); err != nil {
+			t.Errorf("Removing file must not failed, got %q", err)
+		}
+
+		// test with non existing file
+		f, _ = p.TempFile_()
+		f.Remove()
+		if err := f.RemoveP(); err != nil {
+			t.Errorf("Removing non existing file must not return error, got %q", err)
+		}
+
+		// test with no access privilege
+		if err := Path("/root/foo").RemoveP(); !os.IsPermission(err) {
+			t.Errorf(
+				"Path('/root/foo').RemoveP() must return permission error, got %q",
+				err,
+			)
+		}
+	})
+}
+
+func TestRemoveTree(t *testing.T) {
+	TmpDir_(func(p Path) {
+		// test normal behavior
+		d, _ := TempDir_()
+		if err := d.RemoveTree(); err != nil {
+			t.Errorf("Removing a dir must not return an error, got %q", err)
+		}
+
+		// test removing non existing dir
+		if err := d.RemoveTree(); !os.IsNotExist(err) {
+			t.Errorf("Removing a non existing dir must return an error, got %q", err)
+		}
+	})
+}
+
+func TestRemoveTreeP(t *testing.T) {
+	TmpDir_(func(p Path) {
+		// test normal behavior
+		d, _ := TempDir_()
+		if err := d.RemoveTreeP(); err != nil {
+			t.Errorf("Removing a dir must not return an error, got %q", err)
+		}
+
+		// test removing non existing dir
+		if err := d.RemoveTreeP(); err != nil {
+			t.Errorf(
+				"Removing a non existing dir must not return an error, got %q", err,
 			)
 		}
 	})
